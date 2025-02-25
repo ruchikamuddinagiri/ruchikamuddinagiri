310f36ab5c58d433bb51eef66a81b1290cf49b1a
Ruchika <52284361+ruchikamuddinagiri@users.noreply.github.com>
2025-02-25 05:09:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const fs = require('fs')
-const ejs = require('ejs')
+const fs = require('fs');
+const ejs = require('ejs');
 
-let rawdata = fs.readFileSync('details.json')
-let profile = JSON.parse(rawdata)
-let html = ''
-const parse = async (fileName)=>{
-    html = await ejs.renderFile(fileName, {
-        ...profile,
-        lastRunOn: new Date().toISOString()
-    })
-    return html
-}
+let rawdata = fs.readFileSync('details.json');
+let profile = JSON.parse(rawdata);
+profile.lastRunOn = new Date().toISOString(); // Ensure changes in every run
 
-const main = async ()=>{
-    const htmlString = await parse('index.ejs')
-    console.log(htmlString)
-    fs.writeFile('./README.md', htmlString, ()=>{
-        console.log("file written")
-    })
-}
+const parse = async (fileName) => {
+    return await ejs.renderFile(fileName, profile);
+};
 
-main()
+const main = async () => {
+    const htmlString = await parse('index.ejs');
+    fs.writeFileSync('./README.md', htmlString);
+    console.log("README updated");
+};
 
+main();
